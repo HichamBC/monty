b4cbacd4245cd83ca6ba3b273ca39a5a31230c16
HichamBC <yours.hichambc@gmail.com>
2024-03-28 16:09:20 +0000

    monty program

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+
+
+/**
+ * main - Entry point for the Monty interpreter
+ *
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ *
+ * Return: 0 on successful execution, EXIT_FAILURE if an error occurs.
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	char buffer[100];
+	char *s;
+	unsigned int line = 0;
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(buffer, sizeof(buffer), fp) != NULL)
+	{
+		line++;
+		s = strtok(buffer, " \t\n");
+		if (s != NULL)
+		{
+			execute_instruction(s, &stack, line);
+		}
+	}
+
+	fclose(fp);
+	return (0);
+}
